15a60dbb6d5d71d625ceaafe61e2388cd622b8d7
Zahri27 <privetmurik@gmail.com>
2024-03-07 12:54:52 +0200

    add new System out println

--- src/HomeworkMurad.java
+++ src/HomeworkMurad.java
@@ -3,7 +3,7 @@ public class HomeworkMurad {
     public static void main(String[] args) {
 
        System.out.println("Hello git");
-
+        System.out.println("second changes for git");
 
 
     }
